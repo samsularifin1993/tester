2967bac72586db8d4047a0ffcce1ab182cfc791c
samsularifin1993 <samsularifin.id@gmail.com>
2019-02-20 16:57:07 +0700

    Datatables Button

--- resources/views/frontend/user.blade.php
+++ resources/views/frontend/user.blade.php
@@ -35,12 +35,25 @@
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js" type="text/javascript"></script>
 
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js" type="text/javascript"></script>
+
 <script>
     $(document).ready(function() {
         $.noConflict();
         $('#datatables').DataTable({
             processing: true,
             serverSide: true,
+            dom: 'Bflrtip',
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5'
+            ],
             ajax: '{{ route('user.getAll') }}',
             columns: [
                 { data: 'name',name: 'name'},
